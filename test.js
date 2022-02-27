02bf8e73c0ec194471763e4779232919475b8467
isaacs <i@izs.me>
2022-02-26 20:52:05 -0800

    update things

--- test.js
+++ test.js
@@ -7,4 +7,4 @@ assert.deepEqual(extend({a:1}, true),       {a:1});
 assert.deepEqual(extend({a:1}, false),      {a:1});
 assert.deepEqual(extend({a:1}, {b:2}),      {a:1, b:2});
 assert.deepEqual(extend({a:1, b:2}, {b:3}), {a:1, b:3});
-console.log('ok');
+console.log('TAP version 13\nok\n1..1');
